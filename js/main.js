1444014c97ec0b9bb819c2c41d04f50768f92f46
Adam Perlis <adam.perlis@gmail.com>
2015-09-16 16:43:48 -0400

    second commmit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-$(window).stellar();
+$(window).stellar({
+	responsive: true,  
+	positionProperty: 'transform',
+	hideDistantElements: false
+});
+
+var fadeStart=115 // 100px scroll or less will equiv to 1 opacity
+,fadeUntil=1000 // 200px scroll or more will equiv to 0 opacity
+,fading = $('.images img');
+
+$(window).bind('scroll', function(){
+    var offset = $(document).scrollTop()
+        ,opacity=1;
+
+    // console.log(offset)
+    if( offset<=fadeStart ){
+        opacity=0;
+    }else if( offset<=fadeUntil ){
+       	opacity=0+offset/fadeUntil;
+    }
+    fading.css('opacity',opacity).html(opacity);
+});
+
